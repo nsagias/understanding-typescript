fcf1ed9fdc22b805daa98212c082ef5f6c35a118
Nick <nsagias@gmail.com>
2021-12-10 01:04:08 -0500

    Add typecasting for button element

--- first-project/dist/using-ts.js
+++ first-project/dist/using-ts.js
@@ -1,7 +1,8 @@
-var button = document.querySelector("button");
+"use strict";
+const button = document.querySelector("button");
 // typecasting
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
+const input1 = document.getElementById("num1");
+const input2 = document.getElementById("num2");
 function add(num1, num2) {
     return num1 + num2;
 }
