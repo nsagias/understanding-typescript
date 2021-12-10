d4478da7dda6d7b715a7e490be5b0a10e112ade6
Nick <nsagias@gmail.com>
2021-12-10 15:52:01 -0500

    refactor console.log

--- 01-ts-basics/app.ts
+++ 01-ts-basics/app.ts
@@ -6,7 +6,7 @@ function add(num1:number, num2:number, showResult: boolean): number {
     throw new Error ('runtime type checking');
   }
   if (showResult) {
-    console.log( num1 + num2);
+    console.log(num1 + num2);
   } else {
     return num1 + num2;
   }
@@ -16,5 +16,4 @@ const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
 
-const result = add(number1, number2, printResult);
-console.log("Add result:",result);
+add(number1, number2, printResult);
