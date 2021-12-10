f32619d218f5776ab3b3fb165a0c7f4038ff8cef
Nick <nsagias@gmail.com>
2021-12-10 15:55:27 -0500

    add phrase result

--- 01-ts-basics/app.ts
+++ 01-ts-basics/app.ts
@@ -1,12 +1,12 @@
 interface AddFunc {
-  (num1: number, num2: number, showResult: boolean): number;
+  (num1: number, num2: number, showResult: boolean, phrase: string): number;
 }
-function add(num1:number, num2:number, showResult: boolean): number {
+function add(num1:number, num2:number, showResult: boolean, phrase: string): number {
   if (typeof num1 !== "number" || typeof num2 !== "number") {
     throw new Error ('runtime type checking');
   }
   if (showResult) {
-    console.log(num1 + num2);
+    console.log(phrase + num1 + num2);
   } else {
     return num1 + num2;
   }
@@ -15,5 +15,6 @@ function add(num1:number, num2:number, showResult: boolean): number {
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const phraseResult = "Result is: ";
 
-add(number1, number2, printResult);
+add(number1, number2, printResult, phraseResult);
