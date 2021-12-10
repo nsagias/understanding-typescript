1f6e5e389139cf5c5ce8934e2db22beafda1a3c2
Nick <nsagias@gmail.com>
2021-12-10 13:43:49 -0500

    update to app.js vs index hs

--- 01-ts-basics/app.ts
+++ 01-ts-basics/app.ts
@@ -2,8 +2,8 @@ function add(num1, num2) {
   return num1 + num2;
 }
 
-const number1 = 3;
-const number2 = 2.1;
+const number1 = 5;
+const number2 = 2.8;
 
 const result = add(number1, number1);
 console.log(result);
