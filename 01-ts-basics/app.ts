8252e6dd3916d36c486aac51969074e00ba3f5b5
Nick <nsagias@gmail.com>
2021-12-10 15:47:25 -0500

    Add boolean type to function and interface and add runtime erro catching

--- 01-ts-basics/app.ts
+++ 01-ts-basics/app.ts
@@ -1,12 +1,18 @@
+import { type } from "os";
+
 interface AddFunc {
-  (num1: number, num2: number): number;
+  (num1: number, num2: number, showResult: boolean): number;
 }
-function add(num1:number, num2:number): number {
+function add(num1:number, num2:number, showResult: boolean): number {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    throw new Error ('runtime type checking');
+  }
   return num1 + num2;
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult);
 console.log("Add result:",result);
