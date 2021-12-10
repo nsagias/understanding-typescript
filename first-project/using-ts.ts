f4a2652e11f76d7520861ba9aa3987c86f6f5206
Nick <nsagias@gmail.com>
2021-12-10 00:47:54 -0500

    add ts code to function

--- first-project/using-ts.ts
+++ first-project/using-ts.ts
@@ -3,7 +3,7 @@ const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
 
-function add(num1, num2) {
+function add(num1: number, num2: number): number {
     return num1  + num2;
 }
 
