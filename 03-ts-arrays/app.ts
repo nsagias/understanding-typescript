eaf868715465f2ffb1a3c6d4c37c403e008bc830
Nick <nsagias@gmail.com>
2021-12-10 23:34:18 -0500

    create arrays folder and add array example

---
+++ 03-ts-arrays/app.ts
@@ -0,0 +1,33 @@
+
+interface Person {
+  name: string;
+  age: number;
+  hobbies: [];
+}
+const person1: Person = {
+  name: "Dingo",
+  age: 1000,
+  hobbies: []
+};
+
+
+
+interface Product {
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  }
+}
+
+const product1: Product = {
+  id: 'abc1',
+  price: 12.99,
+  tags: ['great-offer', 'hot-and-new'],
+  details: {
+    title: 'Red Carpet',
+    description: 'A great carpet - almost brand-new!'
+  }
+}
