aca7d15e94bcc57fbee565c974681f3f4698fc2e
Nick <nsagias@gmail.com>
2021-12-11 12:02:28 -0500

    add function expression for add and void

--- 07-ts-function-returns/app.ts
+++ 07-ts-function-returns/app.ts
@@ -13,9 +13,12 @@ function addV2(num1:number, num2:number): number {
 
 const addV3: AddFunc2 = (num1: number, num2: number): number => num1 + num2;
 
+
 // function void type
 function printResult2(num:number): void {
   console.log('Result is: ' + num)
 }
+
+const printResult3: PrintFunc = (num:number): void => undefined;
 
 printResult2(addV2(10,5))
