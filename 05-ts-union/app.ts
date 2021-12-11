02d5f52306e64d609005b9445bbc7b4f1d4dda5a
Nick <nsagias@gmail.com>
2021-12-11 10:25:52 -0500

    add literal types

--- 05-ts-union/app.ts
+++ 05-ts-union/app.ts
@@ -1,10 +1,17 @@
 interface Combine {
-  (input1: number | string, input2: number | string);
+  (input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-string');
 }
 
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string, 
+  input2: number | string, 
+  resultConversion: 'as-number' | 'as-string') {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
+  if (
+    typeof input1 === "number" 
+    && typeof input2 === "number" 
+    || resultConversion === 'as-number') {
+      
     result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
@@ -13,5 +20,5 @@ function combine(input1: number | string, input2: number | string) {
 }
 
 
-const combineAges = combine(30, 26);
-const combineNames = combine("Bob", "toaster")
+const combineAges = combine(30, 26, 'as-number');
+const combineNames = combine("Bob", "toaster", 'as-string')
