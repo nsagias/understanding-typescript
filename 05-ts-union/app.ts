094abbded8e395d05b1d9ae4ff2199ef1f2c63bc
Nick <nsagias@gmail.com>
2021-12-11 01:46:52 -0500

    Add runtime check create if/else to return number or string

--- 05-ts-union/app.ts
+++ 05-ts-union/app.ts
@@ -1,8 +1,17 @@
 interface Combine {
-  (input1: number, input2: number);
+  (input1: number | string, input2: number | string);
 }
 
-function combine(input1:number, input2:number) {
-  const result = input1 + input2;
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
   return result;
 }
+
+
+const combineAges = combine(30, 26);
+const combineNames = combine("Bob", "toaster")
