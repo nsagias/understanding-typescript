95d8f90ffdea4d9c234f51ddd81f45529b839a85
Nick <nsagias@gmail.com>
2021-12-11 10:21:07 -0500

    Add coersion to inputs

--- 05-ts-union/app.ts
+++ 05-ts-union/app.ts
@@ -5,7 +5,7 @@ interface Combine {
 function combine(input1: number | string, input2: number | string) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
