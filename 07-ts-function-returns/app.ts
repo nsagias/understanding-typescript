546c3c4f855ee697d12780628ebf1fefbb2fbd86
Nick <nsagias@gmail.com>
2021-12-11 11:58:23 -0500

    Add functions expression with interface

--- 07-ts-function-returns/app.ts
+++ 07-ts-function-returns/app.ts
@@ -1,8 +1,21 @@
-interface AddFunc {
+interface AddFunc2 {
   (num1: number, num2: number): number;
 }
-function add(num1:number, num2:number): number {
 
+interface PrintFunc {
+  (num: number): void;
+}
+
+function addV2(num1:number, num2:number): number {
   const result = num1 + num2;
   return result;
 }
+
+const addV3: AddFunc2 = (num1: number, num2: number): number => num1 + num2;
+
+// function void type
+function printResult2(num:number): void {
+  console.log('Result is: ' + num)
+}
+
+printResult2(addV2(10,5))
