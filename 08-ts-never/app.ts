facc70b07f5d26326ca3da3ebec96bb88d02977f
Nick <nsagias@gmail.com>
2021-12-11 16:11:53 -0500

    create generatError function for never example

--- 08-ts-never/app.ts
+++ 08-ts-never/app.ts
@@ -1,1 +1,5 @@
-console.log("bob")
+function generateError(message: string, code: number): never {
+  throw {message: message, errorCode: code};
+}
+// utility function
+generateError('Error', 500);
