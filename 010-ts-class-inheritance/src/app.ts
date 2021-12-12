fa1098a608d099c6d635322899cf33061fc9e671
Nick <nsagias@gmail.com>
2021-12-12 18:38:52 -0500

    create class inheritance folder

---
+++ 010-ts-class-inheritance/src/app.ts
@@ -0,0 +1,36 @@
+class Department {
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
+  
+  constructor(private readonly id: string, private name: string) {
+    this.id = id;
+    this.name = name;
+  }
+  describe(this: Department): void {
+    const describeOutput =
+    `Department id: ${this.id}  
+Department name: ${this.name}`;
+    console.log(describeOutput);
+  }
+
+  addEmployee(employee:string): void {
+    // this.employees.push(employee);
+    this.employees = [...this.employees, employee];
+  }
+  
+  printEmployeeInfo(): void {
+    console.log("Number of employees",this.employees.length);
+    console.log("A list of employees",this.employees);
+  }
+}
+
+const marketing = new Department("M", "Marketing");
+
+marketing.describe();
+
+marketing.addEmployee("Dingo");
+marketing.printEmployeeInfo();
+
+marketing.addEmployee("Bingo");
+marketing.printEmployeeInfo();
