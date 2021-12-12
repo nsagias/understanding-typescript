096642b58b01c3da714cc09963a6a64a1e731cd8
Nick <nsagias@gmail.com>
2021-12-12 18:02:02 -0500

    create addEmployee method

--- 09-ts-class/src/app.ts
+++ 09-ts-class/src/app.ts
@@ -1,11 +1,16 @@
 class Department {
   name: string;
+  employees: string[] = [];
   
   constructor(name: string) {
     this.name = name;
   }
   describe(this: Department): void {
     console.log(`Department name is: ${this.name}`);
+  }
+
+  addEmployee(employee:string) {
+    this.employees.push(employee);
   }
 }
 
