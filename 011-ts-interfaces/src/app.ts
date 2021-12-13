d915c33883e80247bf51efb4eab2bba7a1bd834d
Nick <nsagias@gmail.com>
2021-12-12 20:52:41 -0500

    add greet() to interface, create variable with Person interface and add properties

--- 011-ts-interfaces/src/app.ts
+++ 011-ts-interfaces/src/app.ts
@@ -2,5 +2,15 @@ interface Person {
   name: string;
   age: number;
 
-  greet(phrase: string):void;
+  greet(phrase: string): void;
 }
+
+let user1: Person;
+
+user1 = {
+  name: 'Nick',
+  age: 1000,
+  greet(phrase: string) {
+    console.log(`${phrase} ${this.name}`)
+  }
+};
