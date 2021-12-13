295cd73d346c1570df4ac85715a34d1bdcfbd8d4
Nick <nsagias@gmail.com>
2021-12-12 23:29:36 -0500

    Create second interface and class, Wip

--- 011-ts-interfaces/src/app.ts
+++ 011-ts-interfaces/src/app.ts
@@ -1,18 +1,29 @@
-interface Person {
+interface PersonInterface {
   name: string;
-  age: number;
+  // age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+interface SecondInterface {
+  name: string;
+}
+
+class Person implements PersonInterface, SecondInterface {
+   name: string;
+   constructor (name: string) {
+     this.name = name;
+   }
+}
+
+let user1: PersonInterface;
 
 user1 = {
   name: 'Nick',
-  age: 1000,
+  // age: 1000,
   greet(phrase: string) {
     console.log(`${phrase} ${this.name}`);
   }
 };
 
-user1.greet("Hello there");
+user1.greet("Hello there"); 
