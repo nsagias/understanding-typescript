88c2566a2e63a2c9c8b172819d1200d1e56a749a
Nick <nsagias@gmail.com>
2021-12-18 17:09:14 -0500

    create Course goal function

--- 015-ts-generic-utility/src/app.ts
+++ 015-ts-generic-utility/src/app.ts
@@ -5,3 +5,7 @@ interface CourseGoal {
   description: string;
   completeUntil: Date;
 }
+
+function createCourseGoal1(title: string, description: string, date: Date) {
+  return {title: title, description: description, completeUntil: date}
+}
