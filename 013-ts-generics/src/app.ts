5568088d693dd0c28f23423e3a2fda81c578a9aa
Nick <nsagias@gmail.com>
2021-12-17 23:43:49 -0500

    create timeout promise

--- 013-ts-generics/src/app.ts
+++ 013-ts-generics/src/app.ts
@@ -6,7 +6,15 @@ const names5: Array<number> = [];
 const names6: Array<string | number> = [];
 
 // can call string methods and no complaints 
-names4[0].split(' ');
+// names4[0].split(' ');
 
 // promises type
-const promise = new Promise((resolve, reject) => {});
+// const promise = new Promise((resolve, reject) => {});
+const promise = new Promise((resolve) => {
+  setTimeout( () => {
+    resolve('This is done');
+  }, 2000);
+});
+
+promise.then(x => console.log(x));
+
