642c021cc18fcef4f4e6680802a1b706adbce84b
Nick <nsagias@gmail.com>
2021-12-17 23:33:13 -0500

    array types

--- 013-ts-generics/src/app.ts
+++ 013-ts-generics/src/app.ts
@@ -1,1 +1,6 @@
-console.log('Getting started with generics');
+const names1 = ["nick", "bingo"];
+const names2: any = [];
+const names3: Array = [];
+const names4: Array<string> = [];
+const names5: Array<number> = [];
+const names6: Array<string | number> = [];
