6dc3f3907de70874b112318be841111303b491a2
Nick <nsagias@gmail.com>
2021-12-17 23:34:51 -0500

    Call string method on string array

--- 013-ts-generics/src/app.ts
+++ 013-ts-generics/src/app.ts
@@ -1,6 +1,9 @@
 const names1 = ["nick", "bingo"];
 const names2: any = [];
-const names3: Array = [];
+const names3: Array<any> = [];
 const names4: Array<string> = [];
 const names5: Array<number> = [];
 const names6: Array<string | number> = [];
+
+// can call string methods and no complaints 
+names4[0].split(' ');
