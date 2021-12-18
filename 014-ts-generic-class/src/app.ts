b6b3fd342eb9e2236c64444e481a088b34d2b1a1
Nick <nsagias@gmail.com>
2021-12-18 15:33:41 -0500

    Rename class to DataStorage, add getItems method

--- 014-ts-generic-class/src/app.ts
+++ 014-ts-generic-class/src/app.ts
@@ -1,7 +1,15 @@
-class StorageClass {
+class DataStorage {
   private data = [];
 
   addItem(item) {
     this.data.push(item);
+  }
+
+  removeItem(item) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
   }
 }
