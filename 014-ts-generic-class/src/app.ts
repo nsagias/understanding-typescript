c4f690ad7885748a50a66a87e97482c5be15ec44
Nick <nsagias@gmail.com>
2021-12-18 16:05:16 -0500

    update example to work with objects, add copy of DataStorage to DataStorageObjects

--- 014-ts-generic-class/src/app.ts
+++ 014-ts-generic-class/src/app.ts
@@ -35,10 +35,37 @@ console.log(numberStorage.getItems());
 numberStorage.removeItem(2);
 console.log(numberStorage.getItems());
 
+
+
+class DataStorageObjects<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    // this.data.push(item);
+    const newItem = [ ...this.data, item ];
+    this.data = newItem;
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
 // objects example
-const objectStorage = new DataStorage<object>();
-objectStorage.addItem({name: "Nick"});
-objectStorage.addItem({name: "Dingo"});
+const objectStorage = new DataStorageObjects<object>();
+// need to use same pointer reference in memory for objects
+const nickObject = {name: "Nick"};
+objectStorage.addItem(nickObject);
+
+const dingoObject = {name: "Dingo"};
+objectStorage.addItem(dingoObject);
+
 console.log(objectStorage.getItems());
-objectStorage.removeItem({name: "Nick"});
+objectStorage.removeItem(dingoObject);
 console.log(objectStorage.getItems());
